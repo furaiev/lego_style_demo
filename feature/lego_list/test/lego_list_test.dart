c666409d38906c6c5ab9ea621d6626b3ce190d42
Oleksii Furaiev <oleksii.furaiev@tide.co>
2024-07-10 00:51:07 +0300

    codecov scripts

--- feature/lego_list/test/lego_list_test.dart
+++ feature/lego_list/test/lego_list_test.dart
@@ -29,10 +29,10 @@ void main() {
       await iSeeText(tester, 'Lego DC');
     });
 
-    // testWidgets('''I can see world's details''', (tester) async {
-    //   await theFlowIsRunningWithConfiguration(tester, 'list');
-    //   await iTapText(tester, 'Lego Harry Potter');
-    //   await navigationToUniverseIsRequested(tester, 'hp');
-    // });
+    testWidgets('''I can see world's details''', (tester) async {
+      await theFlowIsRunningWithConfiguration(tester, 'list');
+      await iTapText(tester, 'Lego Harry Potter');
+      await navigationToUniverseIsRequested(tester, 'hp');
+    });
   });
 }
