11d1658fdd7e56f7cf2434eb08d1e0b624c8838f
Oleksii <oleksii.furaiev@tide.co>
2024-08-30 11:46:07 +0300

    test long path script

--- .github/workflows/collect_coverage.sh
+++ .github/workflows/collect_coverage.sh
@@ -19,7 +19,7 @@ if [ ! -f dart_test.yaml ]; then
   echo "COVERAGE_FILE is set to: ${COVERAGE_FILE}"
 
   TEMP_FILE=$(mktemp)
-  PROJECT_ROOT=$(pwd)
+  PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
   # Debugging output: Check if coverage file exists after tests
   echo "Checking if ${COVERAGE_FILE} exists..."
@@ -32,7 +32,7 @@ if [ ! -f dart_test.yaml ]; then
   head -n 10 "${COVERAGE_FILE}"
 
   # Update the file paths in the coverage file
-  sed "s|SF:\(.*\)|SF:${PROJECT_ROOT}/\1|g" "${COVERAGE_FILE}" > "${TEMP_FILE}"
+  sed "s|SF:|SF:${PROJECT_ROOT}/|g" "${COVERAGE_FILE}" > "${TEMP_FILE}"
   mv "${TEMP_FILE}" "${COVERAGE_FILE}"
 
   echo "Contents of ${COVERAGE_FILE} after changes:"
